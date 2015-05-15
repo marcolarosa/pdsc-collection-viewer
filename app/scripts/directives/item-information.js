5aa0d6832bf0a3eb5324219a2f4a27b4c951f14d
Marco La Rosa <m@lr.id.au>
2015-05-15 11:28:30 +1000

    restyle view-media layout
    info panel always on screen with media

--- app/scripts/directives/item-information.js
+++ app/scripts/directives/item-information.js
@@ -14,7 +14,8 @@ angular.module('pdscApp')
       scope: {
           itemData: '=',
           show: '=',
-          close: '&'
+          close: '&',
+          alwaysOpen: '@'
       },
       link: function postLink(scope, element, attrs) {
           // handle window resize events
@@ -30,26 +31,36 @@ angular.module('pdscApp')
               scope.width = $window.innerWidth;
               scope.panelHeight = $window.innerHeight;
 
-              scope.backgroundStyle = {
-                  'position': 'fixed',
-                  'top': '0',
-                  'left': '0',
-                  'width': $window.innerWidth,
-                  'height': $window.innerHeight,
-                  'background-color': '#ccc',
-                  'z-index': '100',
-                  'opacity': '0.3',
-              }
-              scope.contentStyle = {
-                  'position': 'fixed',
-                  'top': $window.innerHeight * 0.01 + 'px',
-                  'left': $window.innerWidth * 0.01 + 'px',
-                  'width': $window.innerWidth * 0.4 + 'px', 
-                  'height': $window.innerHeight * 0.9 + 'px',
-                  'z-index': '110',
-                  'border-radius': '2px',
-                  'box-shadow': '5px 5px 4px #888888',
-                  'padding': '15px'
+              if (_.truthy(scope.alwaysOpen)) {
+                  scope.backgroundStyle = {};
+                  scope.contentStyle = { 
+                      'margin': '0px 15px',
+                      'padding': '15px',
+                      'border-radius': '2px',
+                      'box-shadow': '5px 5px 4px #888888'
+                  };
+              } else {
+                  scope.backgroundStyle = {
+                      'position': 'fixed',
+                      'top': '0',
+                      'left': '0',
+                      'width': $window.innerWidth,
+                      'height': $window.innerHeight,
+                      'background-color': '#ccc',
+                      'z-index': '100',
+                      'opacity': '0.3',
+                  }
+                  scope.contentStyle = {
+                      'position': 'fixed',
+                      'top': $window.innerHeight * 0.01 + 'px',
+                      'left': $window.innerWidth * 0.01 + 'px',
+                      'width': $window.innerWidth * 0.4 + 'px', 
+                      'height': $window.innerHeight * 0.9 + 'px',
+                      'z-index': '110',
+                      'border-radius': '2px',
+                      'box-shadow': '5px 5px 4px #888888',
+                      'padding': '15px'
+                  }
               }
           }
           sizeThePanels();
