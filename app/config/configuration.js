052d700a130ed8e26828a7f16aa50db870a6c6a3
Marco La Rosa <m@lr.id.au>
2015-05-15 10:02:57 +1000

    add documentation

--- app/config/configuration.js
+++ app/config/configuration.js
@@ -10,18 +10,26 @@
 angular.module('pdscApp')
   .constant('configuration', {
 
+      // This viewer can be augmented to work for other datasources - defined further on.
+      //  Define those in the section 'datasource' then set the deployment as required.
       "deployment": "paradisec",
 
+      // The datasources.
+      //  The viewer is predicated on the idea that a service can be queried for metadata of an item (which is part
+      //  of a collection) and the application can build a datastructure containing some of the item metadata and the
+      //  content that it defines (images, audio and video).
+      //
+      //  In this section, define methods and configuration snippets required to query and process a datasource, then,
+      //  add a service for that datasource in scripts/services. Probably best to follow the paradisec.js example.
+      //
+      //  Once you have the code to process your datasource (noting that you MUST produce a datastructure as the
+      //  paradisec example - see createItemDataStructure()), integrate it into scripts/controllers/main.js.
       "datasource": {
           "paradisec": {
-              "collections": "http://catalog.paradisec.org.au/collections",
               "itemIdentifier": "oai:paradisec.org.au:{{collectionId}}-{{itemId}}",
               "getItem": "http://catalog.pdsc/oai/item?verb=GetRecord&identifier={{itemId}}&metadataPrefix=olac",
-              "getCollection": "http://catalog.pdsc/oai/item?verb=GetRecord&identifier={{collectionId}}-{{itemId}}&metadataPrefix=olac"
           },
           "esrc": {
-              "getItem": "http://data.esrc.unimelb.edu.au/ead/{{dataset}}/item/{{itemId}}/xml",
-              "getCollection": "http://data.esrc.unimelb.edu.au/ead/{{dataset}}/series/{{collectionId}}/xml"
           },
           "alveo": {
           }
