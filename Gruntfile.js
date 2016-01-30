dfe9690c9a555976acd7c8c0b3ecdb07e66eff83
Marco La Rosa <m@lr.id.au>
2016-01-30 21:15:15 +1100

    use jit-grunt - fix config

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,10 +9,10 @@
 
 module.exports = function (grunt) {
 
-  // Load grunt tasks automatically
-  require('load-grunt-tasks')(grunt);
+  // Time how long tasks take. Can help when optimizing build times
+  require('time-grunt')(grunt);
 
-    // Automatically load required Grunt tasks
+  // Automatically load required Grunt tasks
   require('jit-grunt')(grunt, {
       useminPrepare: 'grunt-usemin',
       ngtemplates: 'grunt-angular-templates',
