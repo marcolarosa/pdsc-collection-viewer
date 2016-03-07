ba53b7b776534e9f0d192c899aba1ee419ee4ce4
Marco La Rosa <m@lr.id.au>
2016-03-07 17:17:45 +1100

    add in moment and supporters

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -37,6 +37,7 @@ module.exports = function(config) {
       'bower_components/videojs/dist/video-js/video.js',
       'bower_components/pdfjs-dist/build/pdf.combined.js',
       'bower_components/pdfjs-dist/web/compatibility.js',
+      'bower_components/moment/moment.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       "app/app/*.js",
