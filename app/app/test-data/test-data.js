a63009ce01553b92c0f3774d94e770567c0fcf2a
Marco La Rosa <m@lr.id.au>
2016-03-11 12:17:10 +1100

    add in a constant to hold test data

---
+++ app/app/test-data/test-data.js
@@ -0,0 +1,66 @@
+'use strict'
+
+angular.module('pdsc')
+    .constant('data', 
+    {
+        'AC2': {
+            'VUNU105': {
+                'audio': {
+                },
+                'audioVisualisations': {
+                },
+                'citation': 'Arthur Capell (collector), Arthur Capell (researcher), Peter Newton (depositor), Joyce Trudinger (compiler), Elizabeth Pearce (data_inputter), 1957; Untitled Unua story, JPEG/PDF/RTF,  2016-03-11. http://catalog.paradisec.org.au/collections/AC2/items/VUNU105',
+                'collectionId': 'AC2',
+                'collectionLink': 'http://catalog.paradisec.org.au/collections/AC2',
+                'contributor': [
+                    {
+                    'name': 'Arthur Capell',
+                    'role': 'compiler'
+                    },
+                    {
+                    'name': 'Arthur Capell',
+                    'role': 'researcher'
+                    },
+                    {
+                    'name': 'Peter Newton',
+                    'role': 'depositor'
+                    },
+                    {
+                    'name': 'Joyce Trudinger',
+                    'role': 'compiler'
+                    },
+                    {
+                    'name': 'Elizabeth Pearce',
+                    'role': 'data_inputter'
+                    }
+                ],
+                'date': '1957-01-01',
+                'description': '1 page typed manuscript. -- Untitled Unua text (story). Collected by Joyce Trudinger. These files were keyboarded by Elizabeth Pearce as AC2-VUNU105-texts.pdf and AC2-VUNU105-texts.rtf. Language as given: Unua',
+                'documents': [
+                    'http://catalog.paradisec.org.au/repository/AC2/VUNU105/AC2-VUNU105-texts.pdf'
+                ],
+                'eaf': {
+                },
+                'identifier': [
+                    'AC2-VUNU105',
+                    'http://catalog.paradisec.org.au/repository/AC2/VUNU105',
+                    'http://paradisec.org.au/fieldnotes/VUNU.htm#VUNU105'
+                ],
+                'images': [
+                    'http://catalog.paradisec.org.au/repository/AC2/VUNU105/AC2-VUNU105-001.jpg'
+                ],
+                'itemId': 'VUNU105',
+                'openAccess': true,
+                'rights': 'Open (subject to agreeing to PDSC access conditions)',
+                'thumbnails': [
+                    'http://catalog.paradisec.org.au/repository/AC2/VUNU105/AC2-VUNU105-001-thumb-PDSC_ADMINjpg'
+                ],
+                'title': 'Untitled Unua story',
+                'trs': {
+                },
+                'video': {
+                }
+            }
+        }
+    }
+)
