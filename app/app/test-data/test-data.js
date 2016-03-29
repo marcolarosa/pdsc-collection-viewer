8b27f3948186bb06e87461b517b2c1bdaa3182dc
Marco La Rosa <m@lr.id.au>
2016-03-30 01:12:26 +1100

    reworked test data

--- app/app/test-data/test-data.js
+++ app/app/test-data/test-data.js
@@ -4,153 +4,192 @@ angular.module('pdsc')
     .constant('data', 
     {
         'AC2': {
-            'VUNU105': {
+            '1': {
                 'audio': {
                 },
                 'audioVisualisations': {
                 },
-                'citation': 'Arthur Capell (collector), Arthur Capell (researcher), Peter Newton (depositor), Joyce Trudinger (compiler), Elizabeth Pearce (data_inputter), 1957; Untitled Unua story, JPEG/PDF/RTF,  2016-03-11. http://catalog.paradisec.org.au/collections/AC2/items/VUNU105',
+                'citation': '',
                 'collectionId': 'AC2',
                 'collectionLink': 'http://catalog.paradisec.org.au/collections/AC2',
                 'contributor': [
                     {
-                    'name': 'Arthur Capell',
-                    'role': 'compiler'
-                    },
-                    {
-                    'name': 'Arthur Capell',
-                    'role': 'researcher'
-                    },
-                    {
-                    'name': 'Peter Newton',
-                    'role': 'depositor'
-                    },
-                    {
-                    'name': 'Joyce Trudinger',
-                    'role': 'compiler'
-                    },
-                    {
-                    'name': 'Elizabeth Pearce',
-                    'role': 'data_inputter'
+                        'name': 'Arthur Capell',
+                        'role': 'compiler'
                     }
                 ],
                 'date': '1957-01-01',
-                'description': '1 page typed manuscript. -- Untitled Unua text (story). Collected by Joyce Trudinger. These files were keyboarded by Elizabeth Pearce as AC2-VUNU105-texts.pdf and AC2-VUNU105-texts.rtf. Language as given: Unua',
+                'description': '',
                 'documents': [
-                    'http://catalog.paradisec.org.au/repository/AC2/VUNU105/AC2-VUNU105-texts.pdf'
                 ],
                 'eaf': {
                 },
                 'identifier': [
-                    'AC2-VUNU105',
-                    'http://catalog.paradisec.org.au/repository/AC2/VUNU105',
-                    'http://paradisec.org.au/fieldnotes/VUNU.htm#VUNU105'
                 ],
                 'images': [
-                    'http://catalog.paradisec.org.au/repository/AC2/VUNU105/AC2-VUNU105-001.jpg'
+                    'http://catalog.paradisec.org.au/repository/AC2/1/AC2-1-001.jpg'
                 ],
-                'itemId': 'VUNU105',
+                'itemId': '1',
                 'openAccess': true,
                 'rights': 'Open (subject to agreeing to PDSC access conditions)',
                 'thumbnails': [
-                    'http://catalog.paradisec.org.au/repository/AC2/VUNU105/AC2-VUNU105-001-thumb-PDSC_ADMINjpg'
+                    'http://catalog.paradisec.org.au/repository/AC2/1/AC2-1-001-thumb-PDSC_ADMINjpg'
                 ],
                 'title': 'Untitled Unua story',
                 'trs': {
                 },
                 'video': {
                 }
-            }
-        },
-
-        'AA2': {
-            '003': {
+            },
+            '2': {
                 'audio': {
-                    'AA2-003-A': 'http://catalog.paradisec.org.au/repository/AA2/003/AA2-003-A.mp3',
-                    'AA2-003-B': 'http://catalog.paradisec.org.au/repository/AA2/003/AA2-003-B.mp3'
+                    'AC1-1-001': 'http://catalog.paradisec.org.au/repository/AC2/2/AC2-2-001.mp3',
                 },
                 'audioVisualisations': {
-                    'AA2-003-A': 'http://catalog.paradisec.org.au/repository/AA2/003/AA2-003-A-soundimage-PDSC_ADMIN.jpg',
-                    'AA2-003-B': 'http://catalog.paradisec.org.au/repository/AA2/003/AA2-003-B-soundimage-PDSC_ADMIN.jpg'
                 },
-                'citation': "Alexander Adelaar (collector), Alexander Adelaar (recorder), 1990; Continuation of 'Ule'-ule'an Titilong' till end + 'Ule'-ule'an Ma' Malui', MPEG/X-WAV,  2016-03-30. DOI: 10.4225/72/56E97AB7221A6",
-                'collectionId': 'AA2',
+                'citation': '',
+                'collectionId': 'AC2',
                 'collectionLink': 'http://catalog.paradisec.org.au/collections/AC2',
                 'contributor': [
                     {
-                        'name': 'Alexander Adelaar',
+                        'name': 'Arthur Capell',
                         'role': 'compiler'
-                    },
-                    {
-                        'name': 'Alexander Adelaar',
-                        'role': 'recorder'
                     }
                 ],
-                'date': '1990-07-05',
-                'description': "AA2-003 contains  the second and last part of the 'Ule'-ule'an Titilong' story, which  follows on from the part recorded in AA2-005. Language as given: Embaloh",
+                'date': '1957-01-01',
+                'description': '',
                 'documents': [
                 ],
                 'eaf': {
                 },
                 'identifier': [
-                    "AA2-003",
-                    "http://catalog.paradisec.org.au/repository/AA2/003"
                 ],
                 'images': [
                 ],
-                'itemId': '003',
+                'itemId': '2',
                 'openAccess': true,
                 'rights': 'Open (subject to agreeing to PDSC access conditions)',
                 'thumbnails': [
                 ],
-                'title': "Continuation of 'Ule'-ule'an Titilong' till end + 'Ule'-ule'an Ma' Malui'",
+                'title': 'Untitled Unua story',
                 'trs': {
                 },
                 'video': {
                 }
-            }
-        },
-
-        'EAG1': {
-            'S1': {
+            },
+            '3': {
                 'audio': {
-                    "EAG1-S1-wad20110702IMK": "http://catalog.paradisec.org.au/repository/EAG1/S1/EAG1-S1-wad20110702IMK.mp3"
                 },
                 'audioVisualisations': {
-                    "EAG1-S1-wad20110702IMK": "http://catalog.paradisec.org.au/repository/EAG1/S1/EAG1-S1-wad20110702IMK-soundimage-PDSC_ADMIN.jpg"
                 },
-                'citation': "Emily Gasser (collector), 2011; Session 1, X-WAV/MPEG/XML,  2016-03-30. http://catalog.paradisec.org.au/collections/EAG1/items/S1",
-                'collectionId': 'EAG1',
-                'collectionLink': 'http://catalog.paradisec.org.au/collections/EAG1',
+                'citation': '',
+                'collectionId': 'AC2',
+                'collectionLink': 'http://catalog.paradisec.org.au/collections/AC2',
                 'contributor': [
                     {
-                        'name': 'Emily Gasser',
+                        'name': 'Arthur Capell',
                         'role': 'compiler'
                     }
                 ],
-                'date': '2011-07-02',
-                'description': "AA2-003 contains  the second and last part of the 'Ule'-ule'an Titilong' story, which  follows on from the part recorded in AA2-005. Language as given: Embaloh",
+                'date': '1957-01-01',
+                'description': '',
+                'documents': [
+                    'http://catalog.paradisec.org.au/repository/AC2/3/AC2-3-001.pdf'
+                ],
+                'eaf': {
+                },
+                'identifier': [
+                ],
+                'images': [
+                ],
+                'itemId': '3',
+                'openAccess': true,
+                'rights': 'Open (subject to agreeing to PDSC access conditions)',
+                'thumbnails': [
+                ],
+                'title': 'Untitled Unua story',
+                'trs': {
+                },
+                'video': {
+                }
+            },
+            '4': {
+                'audio': {
+                },
+                'audioVisualisations': {
+                },
+                'citation': '',
+                'collectionId': 'AC2',
+                'collectionLink': 'http://catalog.paradisec.org.au/collections/AC2',
+                'contributor': [
+                    {
+                    'name': 'Arthur Capell',
+                    'role': 'compiler'
+                    }
+                ],
+                'date': '1957-01-01',
+                'description': '',
                 'documents': [
                 ],
                 'eaf': {
                 },
                 'identifier': [
-                    "AA2-003",
-                    "http://catalog.paradisec.org.au/repository/AA2/003"
                 ],
                 'images': [
                 ],
-                'itemId': '003',
+                'itemId': 'VUNU105',
                 'openAccess': true,
                 'rights': 'Open (subject to agreeing to PDSC access conditions)',
                 'thumbnails': [
                 ],
-                'title': "Session 1",
+                'title': 'Untitled Unua story',
                 'trs': {
                 },
                 'video': {
+                    'AC2-4-001': 'http://catalog.paradisec.org.au/repository/AC2/4/AC2-4-001.mp4'
+                }
+            },
+            '5': {
+                'audio': {
+                    'AC1-1-001': 'http://catalog.paradisec.org.au/repository/AC2/2/AC2-2-001.mp3',
+                },
+                'audioVisualisations': {
+                },
+                'citation': '',
+                'collectionId': 'AC2',
+                'collectionLink': 'http://catalog.paradisec.org.au/collections/AC2',
+                'contributor': [
+                    {
+                    'name': 'Arthur Capell',
+                    'role': 'compiler'
+                    }
+                ],
+                'date': '1957-01-01',
+                'description': '',
+                'documents': [
+                    'http://catalog.paradisec.org.au/repository/AC2/3/AC2-3-001.pdf'
+                ],
+                'eaf': {
+                },
+                'identifier': [
+                ],
+                'images': [
+                    'http://catalog.paradisec.org.au/repository/AC2/1/AC2-1-001.jpg'
+                ],
+                'itemId': 'VUNU105',
+                'openAccess': true,
+                'rights': 'Open (subject to agreeing to PDSC access conditions)',
+                'thumbnails': [
+                    'http://catalog.paradisec.org.au/repository/AC2/1/AC2-1-001-thumb-PDSC_ADMINjpg'
+                ],
+                'title': 'Untitled Unua story',
+                'trs': {
+                },
+                'video': {
+                    'AC2-4-001': 'http://catalog.paradisec.org.au/repository/AC2/4/AC2-4-001.mp4'
                 }
             }
         }
+
     }
 )
