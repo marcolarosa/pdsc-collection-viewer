cad1497d77f75c7634583d827f496b323baa2da2
Marco La Rosa <m@lr.id.au>
2016-03-30 00:38:32 +1100

    test loading a specific document

--- app/app/components/main/main.spec.js
+++ app/app/components/main/main.spec.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var $controller, $scope, $routeParams, $log, paradisec, configuration, data;
-function setup(collectionId, itemId) {
+function setup(collectionId, itemId, instanceId) {
       beforeEach(module('pdsc'));
       beforeEach(inject(function (
         _$controller_, _$rootScope_, _$log_, _configuration_, _paradisec_, _, _data_
@@ -14,7 +14,10 @@ function setup(collectionId, itemId) {
           configuration = _configuration_;
           data = _data_;
 
-          $scope.itemData = data[collectionId][itemId];
+          $routeParams.collectionId = collectionId;
+          $routeParams.itemId = itemId;
+          $routeParams.itemInstance = instanceId;
+
           $controller('MainCtrl', { 
               '$scope': $scope,
               '$log': $log,
@@ -23,6 +26,7 @@ function setup(collectionId, itemId) {
               'paradisec': paradisec,
               '_': _
           });
+          $scope.itemData = data[collectionId][itemId];
       }));
 }
 
@@ -47,6 +51,30 @@ describe('Controller: MainCtrl', function () {
           expect($scope.loadMediaPlayer).toBe(true);
       });
 
+  });
+
+  describe('test document loading', function() {
+      setup('AC2', 'VUNU105');
+      it('should select the document viewer', function() {
+          $scope.loadViewer('documents');
+          expect($scope.loadDocumentViewer).toBe(true);
+      });
+  });
+
+  describe('test loading a specific image', function() {
+      setup('AC2', 'VUNU105', 'AC2-VUNU105-001');
+      it('should load the image viewer', function() {
+          $scope.whichViewer();
+          expect($scope.loadImageViewer).toBe(true);
+      })
+  });
+
+  describe('test loading a specific document', function() {
+      setup('AC2', 'VUNU105', 'AC2-VUNU105-texts');
+      it('should load the document viewer', function() {
+          $scope.whichViewer();
+          expect($scope.loadDocumentViewer).toBe(true);
+      })
   });
 
 
