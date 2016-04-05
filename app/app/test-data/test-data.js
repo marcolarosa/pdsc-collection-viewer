ec4a00d6d94e58a15014f40e97388b4093d305b8
Marco La Rosa <m@lr.id.au>
2016-04-05 21:57:12 +1000

    test data additions and fixups

--- app/app/test-data/test-data.js
+++ app/app/test-data/test-data.js
@@ -43,7 +43,7 @@ angular.module('pdsc')
             },
             '2': {
                 'audio': {
-                    'AC2-1-001': [ 'http://catalog.paradisec.org.au/repository/AC2/AC2-1-001.mp3' ],
+                    'AC2-2-001': [ 'http://catalog.paradisec.org.au/repository/AC2/2/AC2-2-001.mp3' ],
                 },
                 'audioVisualisations': {
                 },
@@ -66,7 +66,7 @@ angular.module('pdsc')
                 ],
                 'images': [
                 ],
-                'itemId': '1',
+                'itemId': '2',
                 'openAccess': true,
                 'rights': 'Open (subject to agreeing to PDSC access conditions)',
                 'thumbnails': [
@@ -137,7 +137,7 @@ angular.module('pdsc')
                 ],
                 'images': [
                 ],
-                'itemId': 'VUNU105',
+                'itemId': '4',
                 'openAccess': true,
                 'rights': 'Open (subject to agreeing to PDSC access conditions)',
                 'thumbnails': [
@@ -146,12 +146,14 @@ angular.module('pdsc')
                 'trs': {
                 },
                 'video': {
-                    'AC2-4-001': [ 'http://catalog.paradisec.org.au/repository/AC2/4/AC2-4-001.mp4' ]
+                    'AC2-4-001': [ 'http://catalog.paradisec.org.au/repository/AC2/4/AC2-4-001.mp4',
+                                   'http://catalog.paradisec.org.au/repository/AC2/4/AC2-4-001.mp3' ],
+                    'AC2-4-002': [ 'http://catalog.paradisec.org.au/repository/AC2/4/AC2-4-002.mp4' ]
                 }
             },
             '5': {
                 'audio': {
-                    'AC1-1-001': [ 'http://catalog.paradisec.org.au/repository/AC2/2/AC2-2-001.mp3' ],
+                    'AC2-5-001': [ 'http://catalog.paradisec.org.au/repository/AC2/5/AC2-5-001.mp3' ],
                 },
                 'audioVisualisations': {
                 },
@@ -167,26 +169,26 @@ angular.module('pdsc')
                 'date': '1957-01-01',
                 'description': '',
                 'documents': [
-                    'http://catalog.paradisec.org.au/repository/AC2/3/AC2-3-001.pdf'
+                    'http://catalog.paradisec.org.au/repository/AC2/5/AC2-5-001.pdf'
                 ],
                 'eaf': {
                 },
                 'identifier': [
                 ],
                 'images': [
-                    'http://catalog.paradisec.org.au/repository/AC2/1/AC2-1-001.jpg'
+                    'http://catalog.paradisec.org.au/repository/AC2/5/AC2-5-001.jpg'
                 ],
-                'itemId': 'VUNU105',
+                'itemId': '5',
                 'openAccess': true,
                 'rights': 'Open (subject to agreeing to PDSC access conditions)',
                 'thumbnails': [
-                    'http://catalog.paradisec.org.au/repository/AC2/1/AC2-1-001-thumb-PDSC_ADMIN.jpg'
+                    'http://catalog.paradisec.org.au/repository/AC2/5/AC2-5-001-thumb-PDSC_ADMIN.jpg'
                 ],
                 'title': 'Untitled Unua story',
                 'trs': {
                 },
                 'video': {
-                    'AC2-4-001': [ 'http://catalog.paradisec.org.au/repository/AC2/4/AC2-4-001.mp4' ]
+                    'AC2-5-001': [ 'http://catalog.paradisec.org.au/repository/AC2/5/AC2-5-001.mp4' ]
                 }
             },
             '6': {
@@ -212,19 +214,19 @@ angular.module('pdsc')
                 'identifier': [
                 ],
                 'images': [
-                    'http://catalog.paradisec.org.au/repository/AC2/1/AC2-1-001.jpg',
-                    'http://catalog.paradisec.org.au/repository/AC2/1/AC2-1-002.jpg',
-                    'http://catalog.paradisec.org.au/repository/AC2/1/AC2-1-003.jpg',
-                    'http://catalog.paradisec.org.au/repository/AC2/1/AC2-1-004.jpg'
+                    'http://catalog.paradisec.org.au/repository/AC2/6/AC2-6-001.jpg',
+                    'http://catalog.paradisec.org.au/repository/AC2/6/AC2-6-002.jpg',
+                    'http://catalog.paradisec.org.au/repository/AC2/6/AC2-6-003.jpg',
+                    'http://catalog.paradisec.org.au/repository/AC2/6/AC2-6-004.jpg'
                 ],
-                'itemId': '1',
+                'itemId': '6',
                 'openAccess': true,
                 'rights': 'Open (subject to agreeing to PDSC access conditions)',
                 'thumbnails': [
-                    'http://catalog.paradisec.org.au/repository/AC2/1/AC2-1-001-thumb-PDSC_ADMIN.jpg',
-                    'http://catalog.paradisec.org.au/repository/AC2/1/AC2-1-002-thumb-PDSC_ADMIN.jpg',
-                    'http://catalog.paradisec.org.au/repository/AC2/1/AC2-1-003-thumb-PDSC_ADMIN.jpg',
-                    'http://catalog.paradisec.org.au/repository/AC2/1/AC2-1-004-thumb-PDSC_ADMIN.jpg'
+                    'http://catalog.paradisec.org.au/repository/AC2/6/AC2-6-001-thumb-PDSC_ADMIN.jpg',
+                    'http://catalog.paradisec.org.au/repository/AC2/6/AC2-6-002-thumb-PDSC_ADMIN.jpg',
+                    'http://catalog.paradisec.org.au/repository/AC2/6/AC2-6-003-thumb-PDSC_ADMIN.jpg',
+                    'http://catalog.paradisec.org.au/repository/AC2/6/AC2-6-004-thumb-PDSC_ADMIN.jpg'
                 ],
                 'title': 'Untitled Unua story',
                 'trs': {
