1823707bdd633eada5f7599765eee1ebe8a09be6
Marco La Rosa <m@lr.id.au>
2016-04-05 13:38:06 +1000

    fix; remove isolate scope - prevented it from applying

--- app/app/components/main/size-to-parent/size-to-parent.js
+++ app/app/components/main/size-to-parent/size-to-parent.js
@@ -1,22 +1,30 @@
 'use strict';
 
 angular.module('pdsc')
-  .directive('sizeToParent', [ '$window', 'configuration', '$timeout', 
-    function ($window, conf, $timeout) {
+  .directive('sizeToParent', [ '$window', 'configuration', 
+    function ($window, conf) {
     return {
       template: '',
       restrict: 'A',
-      scope: {
-      },
-      link: function postLink(scope, element) {
-          scope.panelStyle = {
-              'position': 'relative',
-              'height': $window.innerHeight - conf.header.toolbar - 
-                            conf.header.headline - conf.header.controls + 'px',
-              'overflow': 'scroll'
-          }
-          console.log(scope.panelStyle);
-          console.log(element);
+      link: function postLink(scope) {
+
+          var sizeThePanel = function() {
+              scope.panelStyle = {
+                  'position': 'relative',
+                  'height': $window.innerHeight - conf.header.toolbar - 
+                                conf.header.headline - conf.header.controls - 30 + 'px',
+                  'overflow': 'scroll'
+              };
+          };
+          sizeThePanel();
+
+          var w = angular.element($window);
+          w.bind('resize', function() {
+              scope.$apply(function() {
+                  sizeThePanel();
+              });
+          });
+
       }
     };
   }]);
