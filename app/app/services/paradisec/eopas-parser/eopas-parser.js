5a7d2bcb7eed2ca6d4facc4a4e33e0cf5f968e1a
Marco La Rosa <m@lr.id.au>
2016-04-18 16:44:33 +1000

    masaging the xml into a suitable format

--- app/app/services/paradisec/eopas-parser/eopas-parser.js
+++ app/app/services/paradisec/eopas-parser/eopas-parser.js
@@ -8,6 +8,29 @@ angular.module('pdsc')
 
       // parse an eopas XML file and return an object keyed on timeslot id
       eopasParser.parse = function(data) {
+          var text = _.map(data.eopas.interlinear.phrase, function(d) {
+              var words = _.map(d.wordlist.word, function(w) {
+                  console.log(w.text, w.morphemelist);
+                  _.each(w.morphemelist, function(m) {
+                      if (_.isPlainObject(m)) {
+                          m = [m];
+                      }
+                      console.log(m);
+                  });
+              });
+              return {
+                'transcription': d.transcription['#text'],
+                'translation': d.translation['#text'],
+                'metadata': {
+                    'id': d['@attributes'].id,
+                    'start': d['@attributes'].startTime,
+                    'end': d['@attributes'].endTime
+                },
+                'words': words
+              }
+          });
+          console.log(data.eopas.interlinear.phrase);
+          console.log(text);
       };
 
       return eopasParser;
