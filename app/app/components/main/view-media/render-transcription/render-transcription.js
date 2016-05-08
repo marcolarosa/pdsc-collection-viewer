171c4caaf52abb71bba3affe90b76fb27171630a
Marco La Rosa <m@lr.id.au>
2016-05-09 08:57:50 +1000

    refactor, handle route params defining fragment to select

--- app/app/components/main/view-media/render-transcription/render-transcription.js
+++ app/app/components/main/view-media/render-transcription/render-transcription.js
@@ -6,7 +6,8 @@ angular.module('pdsc')
     '$anchorScroll', 
     '_',
     '$timeout',
-    function ($location, $anchorScroll, _, $timeout) {
+    '$routeParams',
+    function ($location, $anchorScroll, _, $timeout, $routeParams) {
     return {
       templateUrl: 'app/components/main/view-media/render-transcription/render-transcription.html',
       restrict: 'E',
@@ -24,36 +25,31 @@ angular.module('pdsc')
           scope.selected = {};
           scope.available = {};
 
-          scope.$watch('currentTime', function() {
-              if (scope.currentTime) {
-                  var d;
-                  if (scope.transcript) {
-                      d = scope.transcript;
-                  } else if (scope.eopas) {
-                      d = scope.eopas;
+          scope.set = function() {
+              if ($routeParams.type && $routeParams.segment && $routeParams.selected) {
+                  if ($routeParams.type === 'transcript') {
+                      scope.st = true;
+                      scope.selected.transcript = $routeParams.selected; 
+                  } else if ($routeParams.type === 'interlinear') {
+                      scope.se = true;
+                      scope.selected.interlinear = $routeParams.selected;
                   }
-                  var transformed = _.map(d, function(t) {
-                      if (t.time > scope.currentTime) { 
-                          return 1;
-                      } else {
-                          return 0;
-                      }
-                  });
-                  scope.selectedIndex = _.indexOf(transformed, 1, true) - 1;
-                  var o = $location.hash();
-                  $location.hash(scope.name + '_' + scope.selectedIndex);
-                  $anchorScroll();
-                  $location.hash(o);
-
+                  scope.currentTime = $routeParams.segment;
+              } else {
+                  try {
+                      scope.selected.transcript = scope.available.transcripts[0];
+                      scope.selected.interlinear = scope.available.interlinear[0];
+                  } catch (e) {
+                  }
               }
-          });
+          };
 
           scope.$watch('transcription', function() {
               if (scope.transcription === undefined) { 
                   return;
               }
               scope.available.transcripts = _.keys(scope.transcription).sort();
-              scope.selected.transcript = scope.available.transcripts[0];
+              scope.set()
               scope.load('transcript');
               if (scope.available.transcripts.length > 1) {
                   scope.transcriptsSelector = true;
@@ -65,7 +61,7 @@ angular.module('pdsc')
                   return;
               }
               scope.available.interlinear = _.keys(scope.eopas).sort();
-              scope.selected.interlinear = scope.available.interlinear[0];
+              scope.set();
               scope.load('interlinear');
               if (scope.available.interlinear.length > 1) {
                   scope.interlinearTextsSelector = true;
