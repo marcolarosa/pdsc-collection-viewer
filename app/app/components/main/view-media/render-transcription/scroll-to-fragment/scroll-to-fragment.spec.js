0cf2716ae6cf8b21838c1571a92cc977be7de241
Marco La Rosa <m@lr.id.au>
2016-05-09 08:57:03 +1000

    extract code to scroll to fragment into directive

---
+++ app/app/components/main/view-media/render-transcription/scroll-to-fragment/scroll-to-fragment.spec.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var $compile, $rootScope, element, scope;
+
+var setup = function(collectionId, itemId, name) {
+    beforeEach(module('pdsc'));
+    beforeEach(module('my.templates'));
+
+    beforeEach(inject(function (_$compile_, _$rootScope_, _data_) {
+        $compile = _$compile_;
+        $rootScope = _$rootScope_;
+
+        scope = $rootScope.$new();
+        scope.itemData = _data_[collectionId][itemId];
+        scope.name = name;
+        scope.src = scope.itemData.audio[scope.name];
+        scope.trs = scope.itemData.trs[scope.name];
+        scope.currentTime = 0;
+
+        element = $compile('<render-transcription transcription="trs" name="name" current-time="currentTime"></render-transcription')(scope);
+        scope.$digest();
+        scope = element.isolateScope();
+    }));
+};
+
+describe('Directive: fragment citation', function () {
+});
