2e728a4058e69855452909266cede4a6ea28c2ad
Marco La Rosa <m@lr.id.au>
2016-07-17 09:53:37 +1000

    rejig to include a transcription selector

--- app/app/components/main/view-media/render-transcription/render-transcription.js
+++ app/app/components/main/view-media/render-transcription/render-transcription.js
@@ -5,7 +5,9 @@ angular.module('pdsc')
     '_',
     '$timeout',
     '$routeParams',
-    function (_, $timeout, $routeParams) {
+    '$location',
+    '$anchorScroll',
+    function (_, $timeout, $routeParams, $location, $anchorScroll) {
     return {
       templateUrl: 'app/components/main/view-media/render-transcription/render-transcription.html',
       restrict: 'E',
@@ -19,10 +21,32 @@ angular.module('pdsc')
       },
       link: function postLink(scope) {
           scope.highlight = {};
-          scope.st = false;
-          scope.se = false;
           scope.selected = {};
           scope.available = {};
+
+          scope.$watch('currentTime', function() {
+              if (scope.currentTime) {
+                  var d;
+                  if (scope.transcript) {
+                      d = scope.transcript;
+                  } else if (scope.eopas) {
+                      d = scope.eopas;
+                  }
+                  var transformed = _.map(d, function(t) {
+                      if (t.time > scope.currentTime) {
+                          return 1;
+                      } else {
+                          return 0;
+                      }
+                  });
+                  scope.selectedIndex = _.indexOf(transformed, 1, true) - 1;
+                  var o = $location.hash();
+                  $location.hash(scope.name + '_' + scope.selectedIndex);
+                  $anchorScroll();
+                  $location.hash(o);
+
+              }
+          });
 
           scope.set = function() {
               if ($routeParams.type && $routeParams.segment && $routeParams.selected) {
