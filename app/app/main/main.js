cd6bd40a5741017dd86ad3c5bbde506faf055259
Marco La Rosa <m@lr.id.au>
2018-03-06 12:11:56 +1100

    convert to 1.6 component

--- app/app/main/main.js
+++ app/app/main/main.js
@@ -8,9 +8,8 @@ module.exports = {
 };
 
 Controller.$inject = [
-  'Scope',
+  '$state',
   '$log',
-  '$routeParams',
   'configuration',
   'dataService',
   'lodash',
@@ -18,131 +17,127 @@ Controller.$inject = [
 ];
 
 function Controller(
-  $scope,
+  $state,
   $log,
-  $routeParams,
   configuration,
   dataService,
   lodash,
   $mdSidenav
 ) {
-  console.log('init');
-}
+  var vm = this;
 
-// angular.module('pdsc')
-//   .controller('MainCtrl', [
-//     '$scope',
-//     '$log',
-//     '$routeParams',
-//     'configuration',
-//     'paradisec',
-//     '_',
-//     '$mdSidenav',
-//     function ($scope, $log, $routeParams, conf, paradisec, _, $mdSidenav) {
-//         $scope.showItemInformation = false;
-//         $scope.levelup = false;
-//
-//         $log.debug('MainCtrl: $routeParams', $routeParams);
-//         $scope.collectionId = _.has($routeParams, 'collectionId') ? $routeParams.collectionId : undefined;
-//         $scope.itemId       = _.has($routeParams, 'itemId')       ? $routeParams.itemId       : undefined;
-//         $scope.instanceId   = _.has($routeParams, 'itemInstance') ? $routeParams.itemInstance : undefined;
-//         $log.debug('MainCtrl: ', $scope.collectionId, ' ::: ', $scope.itemId, ' ::: ', $scope.instanceId);
-//
-//         if ($scope.collectionId && $scope.itemId) {
-//             var d = paradisec.getItem($scope.collectionId, $scope.itemId);
-//             d.then(function(resp) {
-//                 $scope.itemData = resp;
-//                 $scope.whichViewer();
-//             });
-//         } else {
-//             $log.debug('MainCtrl: unknown datasource');
-//         }
-//
-//         // show / hide item information panel
-//         $scope.toggleItemInformation = function() {
-//             $mdSidenav('left').toggle();
-//         };
-//
-//         var filter = function(what) {
-//             if (_.isPlainObject(what)) {
-//                 var values = _.map(what, function(d) {
-//                     return d;
-//                 });
-//                 what = _.flatten(values);
-//             }
-//             var m = _.filter(what, function(d) {
-//                 return d.match($scope.instanceId);
-//             });
-//             if (_.isEmpty(m)) {
-//                 return false;
-//             } else {
-//                 return true;
-//             }
-//         };
-//
-//         // determine which viewer to load based on the route configuration
-//         $scope.whichViewer = function() {
-//             // is a specific instance defined? if so, use this to determine which
-//             //  viewer to load.
-//             if ($scope.instanceId) {
-//                 if (filter($scope.itemData.images)) {
-//                     $scope.itemData.documents = [];
-//                     $scope.itemData.audio = [];
-//                     $scope.itemData.video = [];
-//                     $scope.loadViewer('images');
-//                 } else if (filter($scope.itemData.documents)) {
-//                     $scope.itemData.images = [];
-//                     $scope.itemData.audio = [];
-//                     $scope.itemData.video = [];
-//                     $scope.loadViewer('documents');
-//                 } else if (filter($scope.itemData.audio)) {
-//                     $scope.itemData.images = [];
-//                     $scope.itemData.documents = [];
-//                     $scope.itemData.video = [];
-//                     $scope.loadViewer('media');
-//                 } else if (filter($scope.itemData.video)) {
-//                     $scope.itemData.images = [];
-//                     $scope.itemData.documents = [];
-//                     $scope.itemData.audio = [];
-//                     $scope.loadViewer('media');
-//                 }
-//
-//                 // we're focussed in on one specific item so enable the level up toggle
-//                 $scope.levelup = '#/' + $scope.collectionId + '/' + $scope.itemId;
-//             } else {
-//                 //  Otherwise - images > documents > media
-//                 if (! _.isEmpty($scope.itemData.images)) {
-//                     $scope.loadViewer('images');
-//                 } else if (! _.isEmpty($scope.itemData.documents)) {
-//                     $scope.loadViewer('documents');
-//                 } else {
-//                     $scope.loadViewer('media');
-//                 }
-//             }
-//         };
-//
-//         // load an appropriate viewer
-//         $scope.loadViewer = function(dataType) {
-//             // some of the viewers need to know the header height so they
-//             //  can size themselves accordingly
-//             //$scope.headerHeight = document.getElementById('header').clientHeight;
-//
-//             // always ditch info when loading a viewer
-//             $scope.showItemInformation = false;
-//
-//             // now load the required viewer
-//             if (dataType === 'images') {
-//                 $scope.loadImageViewer = true;
-//                 $scope.loadMediaPlayer = false;
-//                 $scope.loadDocumentViewer = false;
-//             } else if (dataType === 'documents') {
-//                 $scope.loadImageViewer = false;
-//                 $scope.loadMediaPlayer = false;
-//                 $scope.loadDocumentViewer = true;
-//             } else if (dataType === 'media') {
-//                 $scope.loadImageViewer = false;
-//                 $scope.loadMediaPlayer = true;
-//                 $scope.loadDocumentViewer = false;
-//             }
-//         };
-//   }]);
+  vm.showItemInformation = false;
+  vm.levelUp = false;
+
+  vm.$onInit = init;
+  vm.$onDestroy = destroy;
+  vm.toggleItemInformation = toggleItemInformation;
+  vm.whichViewer = whichViewer;
+  vm.loadViewer = loadViewer;
+
+  function init() {
+    vm.collectionId = $state.params.collectionId;
+    vm.itemId = $state.params.itemId;
+    vm.instanceId = $state.params.instanceId;
+    $log.info(vm.collectionId, ' ::: ', vm.itemId, ' ::: ', vm.instanceId);
+    loadItemData();
+  }
+
+  function destroy() {}
+
+  function loadItemData() {
+    dataService.getItem(vm.collectionId, vm.itemId).then(resp => {
+      vm.itemData = resp;
+      console.log(vm.itemData);
+      vm.whichViewer();
+    });
+  }
+
+  // show / hide item information panel
+  function toggleItemInformation() {
+    $mdSidenav('left').toggle();
+  }
+
+  function filter(what) {
+    if (_.isPlainObject(what)) {
+      var values = _.map(what, function(d) {
+        return d;
+      });
+      what = _.flatten(values);
+    }
+    var m = _.filter(what, function(d) {
+      return d.match(vm.instanceId);
+    });
+    if (_.isEmpty(m)) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  // determine which viewer to load based on the route configuration
+  function whichViewer() {
+    // is a specific instance defined? if so, use this to determine which
+    //  viewer to load.
+    if (vm.instanceId) {
+      if (filter(vm.itemData.images)) {
+        vm.itemData.documents = [];
+        vm.itemData.audio = [];
+        vm.itemData.video = [];
+        vm.loadViewer('images');
+      } else if (filter(vm.itemData.documents)) {
+        vm.itemData.images = [];
+        vm.itemData.audio = [];
+        vm.itemData.video = [];
+        vm.loadViewer('documents');
+      } else if (filter(vm.itemData.audio)) {
+        vm.itemData.images = [];
+        vm.itemData.documents = [];
+        vm.itemData.video = [];
+        vm.loadViewer('media');
+      } else if (filter(vm.itemData.video)) {
+        vm.itemData.images = [];
+        vm.itemData.documents = [];
+        vm.itemData.audio = [];
+        vm.loadViewer('media');
+      }
+
+      // we're focussed in on one specific item so enable the level up toggle
+      vm.levelup = '#/' + vm.collectionId + '/' + vm.itemId;
+    } else {
+      //  Otherwise - images > documents > media
+      if (!_.isEmpty(vm.itemData.images)) {
+        vm.loadViewer('images');
+      } else if (!_.isEmpty(vm.itemData.documents)) {
+        vm.loadViewer('documents');
+      } else {
+        vm.loadViewer('media');
+      }
+    }
+  }
+
+  // load an appropriate viewer
+  function loadViewer(dataType) {
+    // some of the viewers need to know the header height so they
+    //  can size themselves accordingly
+    //vm.headerHeight = document.getElementById('header').clientHeight;
+
+    // always ditch info when loading a viewer
+    vm.showItemInformation = false;
+
+    // now load the required viewer
+    if (dataType === 'images') {
+      vm.loadImageViewer = true;
+      vm.loadMediaPlayer = false;
+      vm.loadDocumentViewer = false;
+    } else if (dataType === 'documents') {
+      vm.loadImageViewer = false;
+      vm.loadMediaPlayer = false;
+      vm.loadDocumentViewer = true;
+    } else if (dataType === 'media') {
+      vm.loadImageViewer = false;
+      vm.loadMediaPlayer = true;
+      vm.loadDocumentViewer = false;
+    }
+  }
+}
