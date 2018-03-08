6df1c785aa814e4ba46185087c2b31917f895601
Marco La Rosa <m@lr.id.au>
2018-03-08 14:45:24 +1100

    do nothing if no input

--- app/app/filters/extension.filter.js
+++ app/app/filters/extension.filter.js
@@ -3,8 +3,10 @@
 module.exports = [
   () => {
     return input => {
-      var c = input.split('/').pop();
-      return c.split('.')[1];
+      if (input) {
+        var c = input.split('/').pop();
+        return c.split('.')[1];
+      }
     };
   }
 ];
