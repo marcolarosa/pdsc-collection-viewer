bec228b0af568ae4fe7d51929c1efe925af2bd9f
Marco La Rosa <m@lr.id.au>
2018-04-23 13:39:35 +1000

    add webpack configuration for creating librarybox build

---
+++ webpack.deploy.librarybox.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const path = require('path');
+const webpack = require('webpack');
+const merge = require('webpack-merge');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const common = require('./webpack.common.js');
+
+module.exports = merge(common, {
+  devtool: 'nosources-source-map',
+  mode: 'production',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+    filename: '[name].[chunkHash].bundle.js'
+  },
+  plugins: [
+    new UglifyJSPlugin({sourceMap: true}),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('production')
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: [
+          {
+            loader: 'ifdef-loader',
+            options: {
+              DEPLOY_TESTING: false,
+              DEPLOY_PRODUCTION: false
+            }
+          },
+          {loader: 'babel-loader?presets[]=es2015'}
+        ],
+        exclude: /node_modules|bower_components/
+      }
+    ]
+  }
+});
