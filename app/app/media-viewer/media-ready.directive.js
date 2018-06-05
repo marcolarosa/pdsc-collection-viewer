e109fc44a3c449a603b231b08fb2e5e1231c20cb
Marco La Rosa <m@lr.id.au>
2018-06-05 10:08:11 +1000

    remove log in to nabu message and add loading indicator

--- app/app/media-viewer/media-ready.directive.js
+++ app/app/media-viewer/media-ready.directive.js
@@ -8,17 +8,9 @@ module.exports = [
       template: '',
       restrict: 'A',
       link: function(scope, element) {
-        $timeout(() => {
-          if (!scope.vm.mediaReadyToPlay) {
-            scope.vm.loginRequired = true;
-            dataService.userLoggedIn = false;
-          }
-        }, 5000);
         element.on('canplaythrough', () => {
           scope.$apply(function() {
             scope.vm.mediaReadyToPlay = true;
-            scope.vm.loginRequired = false;
-            dataService.userLoggedIn = true;
           });
         });
       }
