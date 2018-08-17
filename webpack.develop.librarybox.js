ae994adbb557f7898a15eb382dceafef9491ae8a
Marco La Rosa <m@lr.id.au>
2018-08-17 16:07:30 +1000

    add missing content path so we can get to repository index

--- webpack.develop.librarybox.js
+++ webpack.develop.librarybox.js
@@ -12,6 +12,7 @@ module.exports = merge(common, {
     serve: {
         host: "0.0.0.0",
         port: "9000",
+        content: "./dist",
         devMiddleware: {
             writeToDisk: true
         },
