6520f469197cb220358fb1431cdbe2b514ada48f
Marco La Rosa <m@lr.id.au>
2018-08-19 09:50:55 +1000

    hide transcriptions in library box mode

--- app/app/file-viewer/view-files.component.js
+++ app/app/file-viewer/view-files.component.js
@@ -1,65 +1,70 @@
-'use strict';
+"use strict";
 
-const {isEmpty, each, map} = require('lodash');
+const configuration = require("app/main/configuration");
+const { isEmpty, each, map } = require("lodash");
 
 module.exports = {
-  template: require('./view-files.component.html'),
-  bindings: {},
-  controller: Controller,
-  controllerAs: 'vm'
+    template: require("./view-files.component.html"),
+    bindings: {},
+    controller: Controller,
+    controllerAs: "vm"
 };
 
 Controller.$inject = [
-  '$state',
-  '$rootScope',
-  '$scope',
-  '$location',
-  '$timeout',
-  'dataService'
+    "$state",
+    "$rootScope",
+    "$scope",
+    "$location",
+    "$timeout",
+    "dataService"
 ];
 
 function Controller(
-  $state,
-  $scope,
-  $rootScope,
-  $location,
-  $timeout,
-  dataService
+    $state,
+    $scope,
+    $rootScope,
+    $location,
+    $timeout,
+    dataService
 ) {
-  var vm = this;
+    var vm = this;
 
-  var broadcastListener;
+    var broadcastListener;
 
-  vm.$onInit = init;
-  vm.$onDestroy = destroy;
+    vm.$onInit = init;
+    vm.$onDestroy = destroy;
 
-  function init() {
-    vm.loadingData = true;
-    broadcastListener = $rootScope.$on('item data loaded', loadItem);
-    loadItem();
-  }
-
-  function destroy() {
-    broadcastListener();
-  }
-
-  function loadItem() {
-    const collectionId = $state.params.collectionId;
-    const itemId = $state.params.itemId;
-    dataService.getItem(collectionId, itemId).then(processResponse);
-
-    function processResponse(resp) {
-      vm.loadingData = false;
-      if (isEmpty(resp)) {
-        return;
-      }
-      $scope.$apply(() => {
-        vm.item = resp;
-        vm.item.images = vm.item.images.map(i => i.split('/').pop());
-        vm.item.documents = vm.item.documents.map(i => i.split('/').pop());
-        vm.hasAudio = vm.item.media.filter(m => m.type === 'audio');
-        vm.hasVideo = vm.item.media.filter(m => m.type === 'video');
-      });
+    function init() {
+        vm.mode = configuration.datasource.mode;
+        console.log(vm.mode);
+        vm.loadingData = true;
+        broadcastListener = $rootScope.$on("item data loaded", loadItem);
+        loadItem();
     }
-  }
+
+    function destroy() {
+        broadcastListener();
+    }
+
+    function loadItem() {
+        const collectionId = $state.params.collectionId;
+        const itemId = $state.params.itemId;
+        dataService.getItem(collectionId, itemId).then(processResponse);
+
+        function processResponse(resp) {
+            vm.loadingData = false;
+            if (isEmpty(resp)) {
+                return;
+            }
+            $scope.$apply(() => {
+                vm.item = resp;
+                vm.item.images = vm.item.images.map(i => i.split("/").pop());
+                vm.item.documents = vm.item.documents.map(i =>
+                    i.split("/").pop()
+                );
+                vm.hasAudio = vm.item.media.filter(m => m.type === "audio");
+                vm.hasVideo = vm.item.media.filter(m => m.type === "video");
+            });
+        }
+    }
 }
